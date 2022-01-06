8375a3750c62f36d116a15b0d2498cccb5e3f2c2
juliagiebelhausen <julia.giebelhausen@coodoo.de>
2022-01-06 14:56:43 +0100

    2. Geburtstagsliste erstellen

--- lib/screens/birthdays.screen.dart
+++ lib/screens/birthdays.screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class BirthdaysScreen extends StatelessWidget {
   const BirthdaysScreen({Key? key}) : super(key: key);
@@ -7,7 +8,34 @@ class BirthdaysScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Geburtstage")),
-      body: const Center(child: Text("...")),
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text('Julia'),
+            trailing: Text(
+              DateFormat('dd.MM.yyyy').format(
+                DateTime(1993, 4, 16),
+              ),
+            ),
+          ),
+          ListTile(
+            title: const Text('Flo'),
+            trailing: Text(
+              DateFormat('dd.MM.yyyy').format(
+                DateTime(2003, 5, 7),
+              ),
+            ),
+          ),
+          ListTile(
+            title: const Text('Marcel'),
+            trailing: Text(
+              DateFormat('dd.MM.yyyy').format(
+                DateTime(1984, 3, 9),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
