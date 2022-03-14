f11aa9a3c688139e28584e03ba6a50a71c8f2010
Nico Doetsch <nico.doetsch@coodoo.de>
2022-03-14 12:00:20 +0100

    05. Geburtstag hinzufügen Teil 1

--- lib/screens/birthday/widgets/birthday_form.dart
+++ lib/screens/birthday/widgets/birthday_form.dart
@@ -1,10 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geburtstags_app/models/birthday.dart';
 import 'package:geburtstags_app/repositories/birthday.repo.dart';
-import 'package:geburtstags_app/screens/birthday/widgets/date_picker.dart';
-import 'package:geburtstags_app/screens/birthday/widgets/name_field.dart';
-import 'package:intl/intl.dart';
 
 class BirthdayForm extends StatefulWidget {
   const BirthdayForm({Key? key}) : super(key: key);
@@ -14,9 +10,6 @@ class BirthdayForm extends StatefulWidget {
 }
 
 class _BirthdayFormState extends State<BirthdayForm> {
-  TextEditingController dateController = TextEditingController();
-  TextEditingController nameController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,32 +17,29 @@ class _BirthdayFormState extends State<BirthdayForm> {
         title: const Text('Geburtstag'),
         actions: [
           TextButton(
-            onPressed: () {
-              BirthdayRepo().insert(
-                Birthday(
-                  name: nameController.text,
-                  date: DateFormat("dd.MM.yyyy").parse(dateController.text),
-                ),
-              );
-              Navigator.pop(context);
-            },
             child: const Text(
               'Speichern',
               style: TextStyle(color: Colors.white),
             ),
+            onPressed: () {
+              BirthdayRepo().insert(
+                Birthday(
+                  name: "Max Mustermann",
+                  date: DateTime(1996, 06, 09),
+                ),
+              );
+              Navigator.pop(context);
+            },
           ),
         ],
       ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Column(
-            children: [
-              NameField(controller: nameController),
-              const SizedBox(height: 10),
-              DatePicker(controller: dateController),
-            ],
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text("Max Mustermann"),
+            Text("09.06.1996"),
+          ],
         ),
       ),
     );
