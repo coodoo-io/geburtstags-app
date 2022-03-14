09fa7d079eaf581adc1ced4ff1d2369e43b03dcb
Nico Doetsch <nico.doetsch@coodoo.de>
2022-03-14 14:48:46 +0100

    03 Daten modellierung

--- lib/templates/template.dart
+++ lib/templates/template.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geburtstags_app/screens/birthdays.screen.dart';
-import 'package:geburtstags_app/screens/home.screen.dart';
+import 'package:geburtstags_app/screens/dashboard.screen.dart';
 import 'package:geburtstags_app/screens/settings.screen.dart';
 
 class Template extends StatefulWidget {
@@ -37,7 +37,7 @@ class _TemplateState extends State<Template> {
       body: IndexedStack(
         index: _index,
         children: const [
-          HomeScreen(),
+          DashboardScreen(),
           BirthdaysScreen(),
           SettingsScreen(),
         ],
