f5652b196045676559745e742ea16a51ef97a705
Florian Schmitz <schmitz.florian.daniel+github@gmail.com>
2022-03-14 17:13:11 +0100

    07 Dashboard

--- lib/screens/dashboard/dashboard.screen.dart
+++ lib/screens/dashboard/dashboard.screen.dart
@@ -31,7 +31,12 @@ class _DashboardScreenState extends State<DashboardScreen> {
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-            const Text("Anstehende Geburtstage"),
+            const Text(
+              "Anstehende Geburtstage🎉",
+              style: TextStyle(fontSize: 24),
+              overflow: TextOverflow.ellipsis,
+            ),
+            const SizedBox(height: 20),
             Expanded(
               child: ListView.builder(
                 itemCount: nextbirthdays.length,
@@ -41,30 +46,35 @@ class _DashboardScreenState extends State<DashboardScreen> {
                   final daysUntilBirthday = dateTimeUtil.remainingDaysUntilBirthday(birthday.date);
                   final getAge = dateTimeUtil.getAge(birthday.date);
 
-                  return Card(
-                    child: ListTile(
-                      onTap: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => BirthdayDetailScreen(birthday: birthday)),
-                      ).then((value) => setState(() {})),
-                      leading: const CircleAvatar(
-                        child: Icon(Icons.person),
-                        radius: 25,
-                      ),
-                      title: Text(birthday.name),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const SizedBox(height: 5),
-                          Text("Am ${DateFormat('dd.MM').format(birthday.date)}"),
-                          const SizedBox(height: 5),
-                          Text(
-                            "In $daysUntilBirthday Tagen",
-                            style: TextStyle(fontStyle: FontStyle.italic, color: Colors.green.shade700),
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 5.0),
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: ListTile(
+                          onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => BirthdayDetailScreen(birthday: birthday)),
+                          ).then((value) => setState(() {})),
+                          leading: const CircleAvatar(child: Icon(Icons.person), radius: 25),
+                          title: Text(birthday.name),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const SizedBox(height: 5),
+                              Text("Am ${DateFormat('dd.MM').format(birthday.date)}"),
+                              const SizedBox(height: 5),
+                              Text(
+                                "In $daysUntilBirthday Tagen",
+                                style: TextStyle(fontStyle: FontStyle.italic, color: Colors.green.shade700),
+                              ),
+                            ],
                           ),
-                        ],
+                          trailing: Text("$getAge Jahre", style: const TextStyle(fontSize: 18)),
+                        ),
                       ),
-                      trailing: Text("$getAge Jahre"),
+                      elevation: 4,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
                     ),
                   );
                 },
