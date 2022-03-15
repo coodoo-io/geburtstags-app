65b372f7397a1e972e47d96fa632e592ae3cc520
Nico Doetsch <nico.doetsch@coodoo.de>
2022-03-15 11:01:12 +0100

    Feature: Shared Preferences

--- lib/repositories/birthday.repo.dart
+++ lib/repositories/birthday.repo.dart
@@ -14,18 +14,19 @@ class BirthdayRepo {
     loadBirthdaysToSP();
   }
 
-  final List<Birthday> repo = [];
+  final List<Birthday> birthdays = [];
 
   List<Birthday> getBirthdays() {
-    return repo;
+    return birthdays;
   }
 
   List<Birthday> getNextFiveBirthdays() {
     final dateTimeUtil = DateTimeUtil();
-    List<Birthday> nextFiveBirthdays = repo;
+    List<Birthday> nextFiveBirthdays = birthdays;
 
-    nextFiveBirthdays.sort((a, b) =>
-        dateTimeUtil.remainingDaysUntilBirthday(a.date).compareTo(dateTimeUtil.remainingDaysUntilBirthday(b.date)));
+    nextFiveBirthdays.sort((a, b) => dateTimeUtil
+        .remainingDaysUntilBirthday(a.date)
+        .compareTo(dateTimeUtil.remainingDaysUntilBirthday(b.date)));
 
     if (nextFiveBirthdays.length > 5) {
       return nextFiveBirthdays.sublist(0, 5);
@@ -36,15 +37,18 @@ class BirthdayRepo {
   List<Birthday> getTodaysBirthdays() {
     List<Birthday> list = [];
 
-    for (var i = 0; i < repo.length; i++) {
-      if (repo[i].date.day == DateTime.now().day && repo[i].date.month == DateTime.now().month) list.add(repo[i]);
+    for (var i = 0; i < birthdays.length; i++) {
+      if (birthdays[i].date.day == DateTime.now().day &&
+          birthdays[i].date.month == DateTime.now().month) {
+        list.add(birthdays[i]);
+      }
     }
 
     return list;
   }
 
   Birthday insert(Birthday birthday) {
-    repo.add(birthday);
+    birthdays.add(birthday);
     saveBirthdaysToSP();
     return birthday;
   }
@@ -54,19 +58,21 @@ class BirthdayRepo {
   }
 
   void delete(Birthday birthday) {
-    repo.remove(birthday);
+    birthdays.remove(birthday);
   }
 
   Future<void> saveBirthdaysToSP() async {
     final prefs = await SharedPreferences.getInstance();
-    List<String> birthdaysEncoded = repo.map((birthday) => jsonEncode(birthday.toJson())).toList();
+    List<String> birthdaysEncoded =
+        birthdays.map((birthday) => jsonEncode(birthday.toJson())).toList();
     prefs.setStringList("birthdays", birthdaysEncoded);
   }
 
   Future<void> loadBirthdaysToSP() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     final jsonList = sharedPreferences.getStringList("birthdays");
-    final decodedList = jsonList?.map((json) => Birthday.fromJson(jsonDecode(json))).toList();
-    repo.addAll(decodedList ?? []);
+    final decodedList =
+        jsonList?.map((json) => Birthday.fromJson(jsonDecode(json))).toList();
+    birthdays.addAll(decodedList ?? []);
   }
 }
