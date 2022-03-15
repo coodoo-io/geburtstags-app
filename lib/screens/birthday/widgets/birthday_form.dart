edcb139df726330c4de57898344f8d45cb2220ed
Nico Doetsch <nico.doetsch@coodoo.de>
2022-03-15 10:53:11 +0100

    05 Geburtstag hinzufügen

--- lib/screens/birthday/widgets/birthday_form.dart
+++ lib/screens/birthday/widgets/birthday_form.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geburtstags_app/models/birthday.dart';
 import 'package:geburtstags_app/repositories/birthday.repo.dart';
