66c473e3d12a541f36b082f38bc1c960beed1ef3
Nico Doetsch <nico.doetsch@coodoo.de>
2022-03-15 10:55:32 +0100

    06 Geburtstag löschen

--- lib/repositories/birthday.repo.dart
+++ lib/repositories/birthday.repo.dart
@@ -8,23 +8,23 @@ class BirthdayRepo {
   }
 
   BirthdayRepo._internal() {
-    repo.add(Birthday(date: DateTime(2020, 6, 12), name: "Max"));
-    repo.add(Birthday(date: DateTime(1999, 1, 11), name: "Flo"));
-    repo.add(Birthday(date: DateTime(1898, 7, 5), name: "Lena"));
-    repo.add(Birthday(date: DateTime(2021, 9, 12), name: "Julia"));
-    repo.add(Birthday(date: DateTime(2022, 10, 12), name: "Markus"));
-    repo.add(Birthday(date: DateTime(2000, 11, 12), name: "Rüdiger"));
-    repo.add(Birthday(date: DateTime(1989, 12, 12), name: "Marcel"));
+    birthdays.add(Birthday(date: DateTime(2020, 6, 12), name: "Max"));
+    birthdays.add(Birthday(date: DateTime(1999, 1, 11), name: "Flo"));
+    birthdays.add(Birthday(date: DateTime(1898, 7, 5), name: "Lena"));
+    birthdays.add(Birthday(date: DateTime(2021, 9, 12), name: "Julia"));
+    birthdays.add(Birthday(date: DateTime(2022, 10, 12), name: "Markus"));
+    birthdays.add(Birthday(date: DateTime(2000, 11, 12), name: "Rüdiger"));
+    birthdays.add(Birthday(date: DateTime(1989, 12, 12), name: "Marcel"));
   }
 
-  final List<Birthday> repo = [];
+  final List<Birthday> birthdays = [];
 
   List<Birthday> getBirthdays() {
-    return repo;
+    return birthdays;
   }
 
   Birthday insert(Birthday birthday) {
-    repo.add(birthday);
+    birthdays.add(birthday);
     return birthday;
   }
 
@@ -33,6 +33,6 @@ class BirthdayRepo {
   }
 
   void delete(Birthday birthday) {
-    repo.remove(birthday);
+    birthdays.remove(birthday);
   }
 }
