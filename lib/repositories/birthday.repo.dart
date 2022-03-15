dd92a4d6213cefdf6d83019419cd9bd54b516e62
Nico Doetsch <nico.doetsch@coodoo.de>
2022-03-15 10:56:04 +0100

    07 Dashboard

--- lib/repositories/birthday.repo.dart
+++ lib/repositories/birthday.repo.dart
@@ -9,24 +9,24 @@ class BirthdayRepo {
   }
 
   BirthdayRepo._internal() {
-    repo.add(Birthday(date: DateTime(2020, 6, 12), name: "Max"));
-    repo.add(Birthday(date: DateTime(1999, 3, 15), name: "Flo"));
-    repo.add(Birthday(date: DateTime(1898, 7, 5), name: "Lena"));
-    repo.add(Birthday(date: DateTime(2021, 9, 12), name: "Julia"));
-    repo.add(Birthday(date: DateTime(2022, 10, 12), name: "Markus"));
-    repo.add(Birthday(date: DateTime(2000, 11, 12), name: "Rüdiger"));
-    repo.add(Birthday(date: DateTime(1989, 12, 12), name: "Marcel"));
+    birthdays.add(Birthday(date: DateTime(2020, 6, 12), name: "Max"));
+    birthdays.add(Birthday(date: DateTime(1999, 3, 15), name: "Flo"));
+    birthdays.add(Birthday(date: DateTime(1898, 7, 5), name: "Lena"));
+    birthdays.add(Birthday(date: DateTime(2021, 9, 12), name: "Julia"));
+    birthdays.add(Birthday(date: DateTime(2022, 10, 12), name: "Markus"));
+    birthdays.add(Birthday(date: DateTime(2000, 11, 12), name: "Rüdiger"));
+    birthdays.add(Birthday(date: DateTime(1989, 12, 12), name: "Marcel"));
   }
 
-  final List<Birthday> repo = [];
+  final List<Birthday> birthdays = [];
 
   List<Birthday> getBirthdays() {
-    return repo;
+    return birthdays;
   }
 
   List<Birthday> getNextFiveBirthdays() {
     final dateTimeUtil = DateTimeUtil();
-    List<Birthday> nextFiveBirthdays = repo;
+    List<Birthday> nextFiveBirthdays = birthdays;
 
     nextFiveBirthdays.sort((a, b) => dateTimeUtil
         .remainingDaysUntilBirthday(a.date)
@@ -39,7 +39,7 @@ class BirthdayRepo {
   }
 
   Birthday insert(Birthday birthday) {
-    repo.add(birthday);
+    birthdays.add(birthday);
     return birthday;
   }
 
@@ -48,6 +48,6 @@ class BirthdayRepo {
   }
 
   void delete(Birthday birthday) {
-    repo.remove(birthday);
+    birthdays.remove(birthday);
   }
 }
