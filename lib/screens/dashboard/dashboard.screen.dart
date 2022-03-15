8620964ac71eed86fa3995d639a798583065ad6a
Florian Schmitz <schmitz.florian.daniel+github@gmail.com>
2022-03-15 21:03:23 +0100

    07 Dashboard

--- lib/screens/dashboard/dashboard.screen.dart
+++ lib/screens/dashboard/dashboard.screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geburtstags_app/repositories/birthday.repo.dart';
 import 'package:geburtstags_app/screens/birthday/detail/birthday_detail.screen.dart';
-import 'package:geburtstags_app/util/datetime.util.dart';
+import 'package:geburtstags_app/utils/datetime.util.dart';
 import 'package:intl/intl.dart';
 
 class DashboardScreen extends StatefulWidget {
@@ -47,8 +47,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                       itemBuilder: (context, index) {
                         final dateTimeUtil = DateTimeUtil();
                         final birthday = nextbirthdays[index];
-                        final daysUntilBirthday = dateTimeUtil
-                            .remainingDaysUntilBirthday(birthday.date);
+                        final daysUntilBirthday = dateTimeUtil.remainingDaysUntilBirthday(birthday.date);
                         final getAge = dateTimeUtil.getAge(birthday.date);
 
                         return Padding(
@@ -59,38 +58,27 @@ class _DashboardScreenState extends State<DashboardScreen> {
                               child: ListTile(
                                 onTap: () => Navigator.push(
                                   context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          BirthdayDetailScreen(
-                                              birthday: birthday)),
+                                  MaterialPageRoute(builder: (context) => BirthdayDetailScreen(birthday: birthday)),
                                 ).then((value) => setState(() {})),
-                                leading: const CircleAvatar(
-                                    child: Icon(Icons.person), radius: 25),
+                                leading: const CircleAvatar(child: Icon(Icons.person), radius: 25),
                                 title: Text(birthday.name),
                                 subtitle: Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
                                     const SizedBox(height: 5),
-                                    Text(
-                                        "Am ${DateFormat('dd.MM').format(birthday.date)}"),
+                                    Text("Am ${DateFormat('dd.MM').format(birthday.date)}"),
                                     const SizedBox(height: 5),
                                     Text(
-                                      daysUntilBirthday == 1
-                                          ? "In einem Tag"
-                                          : "In $daysUntilBirthday Tagen",
-                                      style: TextStyle(
-                                          fontStyle: FontStyle.italic,
-                                          color: Colors.green.shade700),
+                                      daysUntilBirthday == 1 ? "In einem Tag" : "In $daysUntilBirthday Tagen",
+                                      style: TextStyle(fontStyle: FontStyle.italic, color: Colors.green.shade700),
                                     ),
                                   ],
                                 ),
-                                trailing: Text("$getAge Jahre",
-                                    style: const TextStyle(fontSize: 18)),
+                                trailing: Text("$getAge Jahre", style: const TextStyle(fontSize: 18)),
                               ),
                             ),
                             elevation: 4,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15)),
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
                           ),
                         );
                       },
