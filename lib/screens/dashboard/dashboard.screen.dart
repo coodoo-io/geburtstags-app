ceeeecc9562d53255797c8a5903066885d41cd8c
Florian Schmitz <schmitz.florian.daniel+github@gmail.com>
2022-03-16 15:52:30 +0100

    Revert "09 Provider"
    This reverts commit f224850f771bc6635a641347af6be6dc812e3bd8.

--- lib/screens/dashboard/dashboard.screen.dart
+++ lib/screens/dashboard/dashboard.screen.dart
@@ -51,14 +51,10 @@ class DashboardScreen extends StatelessWidget {
                               child: ListTile(
                                 onTap: () => Navigator.push(
                                   context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          BirthdayDetailScreen(
-                                              birthday: birthday)),
+                                  MaterialPageRoute(builder: (context) => BirthdayDetailScreen(birthday: birthday)),
                                 ),
                                 leading: CircleAvatar(
-                                  child:
-                                      Image.asset("assets/images/default.png"),
+                                  child: Image.asset("assets/images/default.png"),
                                   radius: 25,
                                   backgroundColor: Colors.white,
                                 ),
@@ -67,24 +63,19 @@ class DashboardScreen extends StatelessWidget {
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
                                     const SizedBox(height: 5),
-                                    Text(
-                                        "Am ${DateFormat('dd.MM').format(birthday.date)}"),
+                                    Text("Am ${DateFormat('dd.MM').format(birthday.date)}"),
                                     const SizedBox(height: 5),
                                     const Text(
                                       "Heute Geburtstag",
-                                      style: TextStyle(
-                                          fontStyle: FontStyle.italic,
-                                          color: Colors.red),
+                                      style: TextStyle(fontStyle: FontStyle.italic, color: Colors.red),
                                     ),
                                   ],
                                 ),
-                                trailing: Text("$getAge Jahre",
-                                    style: const TextStyle(fontSize: 18)),
+                                trailing: Text("$getAge Jahre", style: const TextStyle(fontSize: 18)),
                               ),
                             ),
                             elevation: 4,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15)),
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
                           ),
                         );
                       },
@@ -106,8 +97,7 @@ class DashboardScreen extends StatelessWidget {
                       itemBuilder: (context, index) {
                         final dateTimeUtil = DateTimeUtil();
                         final birthday = nextbirthdays[index];
-                        final daysUntilBirthday = dateTimeUtil
-                            .remainingDaysUntilBirthday(birthday.date);
+                        final daysUntilBirthday = dateTimeUtil.remainingDaysUntilBirthday(birthday.date);
                         final getAge = dateTimeUtil.getAge(birthday.date);
 
                         return Padding(
@@ -118,14 +108,10 @@ class DashboardScreen extends StatelessWidget {
                               child: ListTile(
                                 onTap: () => Navigator.push(
                                   context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          BirthdayDetailScreen(
-                                              birthday: birthday)),
+                                  MaterialPageRoute(builder: (context) => BirthdayDetailScreen(birthday: birthday)),
                                 ),
                                 leading: CircleAvatar(
-                                  child:
-                                      Image.asset("assets/images/default.png"),
+                                  child: Image.asset("assets/images/default.png"),
                                   radius: 25,
                                   backgroundColor: Colors.white,
                                 ),
@@ -134,26 +120,19 @@ class DashboardScreen extends StatelessWidget {
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
                                     const SizedBox(height: 5),
-                                    Text(
-                                        "Am ${DateFormat('dd.MM').format(birthday.date)}"),
+                                    Text("Am ${DateFormat('dd.MM').format(birthday.date)}"),
                                     const SizedBox(height: 5),
                                     Text(
-                                      daysUntilBirthday == 1
-                                          ? "In einem Tag"
-                                          : "In $daysUntilBirthday Tagen",
-                                      style: TextStyle(
-                                          fontStyle: FontStyle.italic,
-                                          color: Colors.green.shade700),
+                                      daysUntilBirthday == 1 ? "In einem Tag" : "In $daysUntilBirthday Tagen",
+                                      style: TextStyle(fontStyle: FontStyle.italic, color: Colors.green.shade700),
                                     ),
                                   ],
                                 ),
-                                trailing: Text("$getAge Jahre",
-                                    style: const TextStyle(fontSize: 18)),
+                                trailing: Text("$getAge Jahre", style: const TextStyle(fontSize: 18)),
                               ),
                             ),
                             elevation: 4,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15)),
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
                           ),
                         );
                       },
