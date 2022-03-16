f7fe1a4eddde0d0c9d6b4c834291471b1deefa58
Florian Schmitz <schmitz.florian.daniel+github@gmail.com>
2022-03-16 19:55:47 +0100

    08 Heutige Geburtstage

--- lib/utils/datetime.util.dart
+++ lib/utils/datetime.util.dart
@@ -9,8 +9,21 @@ class DateTimeUtil {
     return nextBirthday.difference(now).inDays + 1;
   }
 
-  int getAge(DateTime birthday) {
-    DateTime now = DateTime.now();
-    return now.year - birthday.year;
+  int getAge(DateTime birthDate) {
+    DateTime currentDate = DateTime.now();
+    int age = currentDate.year - birthDate.year;
+    int currentMonth = currentDate.month;
+    int birthDateMonth = birthDate.month;
+
+    if (birthDateMonth > currentMonth) {
+      age--;
+    } else if (currentMonth == birthDateMonth) {
+      int currentDay = currentDate.day;
+      int birthDateDay = birthDate.day;
+      if (birthDateDay > currentDay) {
+        age--;
+      }
+    }
+    return age;
   }
 }
