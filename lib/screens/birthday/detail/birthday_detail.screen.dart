4b555d8c31289683b233e7517fc0c31d02fcf01b
Florian Schmitz <schmitz.florian.daniel+github@gmail.com>
2022-03-16 20:21:05 +0100

    06 Geburtstag löschen

--- lib/screens/birthday/detail/birthday_detail.screen.dart
+++ lib/screens/birthday/detail/birthday_detail.screen.dart
@@ -1,71 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:geburtstags_app/models/birthday.dart';
+import 'package:geburtstags_app/repositories/birthday.repo.dart';
+import 'package:geburtstags_app/screens/birthday/widgets/birthday_form.dart';
 import 'package:intl/intl.dart';
 
-import '../../../repositories/birthday.repo.dart';
-import '../widgets/birthday_form.dart';
-
 class BirthdayDetailScreen extends StatefulWidget {
-  BirthdayDetailScreen({
+  const BirthdayDetailScreen({
     required this.birthday,
     Key? key,
   }) : super(key: key);
 
-  Birthday birthday;
+  final Birthday birthday;
 
   @override
   State<BirthdayDetailScreen> createState() => _BirthdayDetailScreenState();
 }
 
 class _BirthdayDetailScreenState extends State<BirthdayDetailScreen> {
-  void _showAlertDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("${widget.birthday.name} wirklich löschen?"),
-          actions: <Widget>[
-            TextButton(
-              child: const Text("Abbrechen"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: const Text("Löschen"),
-              onPressed: () {
-                BirthdayRepo().delete(widget.birthday);
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('${widget.birthday.name} gelöscht.')),
-                );
-                Navigator.of(context).pop();
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
+  Birthday? birthday;
 
   @override
   Widget build(BuildContext context) {
+    // Wenn die Detail Seite aufgerufen wid, ist birthday null. Dann wollen wir die übergebenen Daten verwenden.
+    // Wenn wir updaten wollen, z.B. den Namen ändern wollen wir nicht nochmal die übergebenen Daten verwenden sondern die aktualisierten Daten.
+    birthday ??= widget.birthday;
+
+    void _showAlertDialog() {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("${birthday!.name} wirklich löschen?"),
+            actions: <Widget>[
+              TextButton(
+                child: const Text("Abbrechen"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                child: const Text("Löschen"),
+                onPressed: () {
+                  BirthdayRepo().delete(birthday!);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('${birthday!.name} gelöscht.')),
+                  );
+                  Navigator.of(context).pop();
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.birthday.name),
+        title: Text(birthday!.name),
         actions: <Widget>[
-          PopupMenuButton(
-              // add icon, by default "3 dot" icon
-              // icon: Icon(Icons.book)
-              itemBuilder: (context) {
+          PopupMenuButton(itemBuilder: (context) {
             return [
-              PopupMenuItem<int>(
+              const PopupMenuItem<int>(
                 value: 0,
-                child: Text("${widget.birthday.name} bearbeiten"),
+                child: Text("Bearbeiten"),
               ),
-              PopupMenuItem<int>(
+              const PopupMenuItem<int>(
                 value: 1,
-                child: Text("${widget.birthday.name} löschen"),
+                child: Text("Löschen"),
               ),
             ];
           }, onSelected: (value) async {
@@ -75,7 +77,7 @@ class _BirthdayDetailScreenState extends State<BirthdayDetailScreen> {
                   fullscreenDialog: true,
                   builder: (BuildContext context) {
                     return BirthdayForm(
-                      birthday: widget.birthday,
+                      birthday: birthday,
                       isEdit: true,
                     );
                   },
@@ -83,7 +85,7 @@ class _BirthdayDetailScreenState extends State<BirthdayDetailScreen> {
               );
 
               setState(() {
-                widget.birthday = response;
+                birthday = response;
               });
             }
             if (value == 1) {
@@ -103,7 +105,7 @@ class _BirthdayDetailScreenState extends State<BirthdayDetailScreen> {
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 5),
-            Text(widget.birthday.name),
+            Text(birthday!.name),
             // Datum
             const SizedBox(height: 20),
             const Text(
@@ -111,7 +113,7 @@ class _BirthdayDetailScreenState extends State<BirthdayDetailScreen> {
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 5),
-            Text(DateFormat('dd.MM.yyyy').format(widget.birthday.date)),
+            Text(DateFormat('dd.MM.yyyy').format(birthday!.date)),
             const SizedBox(height: 20),
             // Zurück
             ElevatedButton(
