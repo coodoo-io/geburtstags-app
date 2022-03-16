f224850f771bc6635a641347af6be6dc812e3bd8
Nico Doetsch <nico.doetsch@coodoo.de>
2022-03-16 15:46:32 +0100

    09 Provider

--- lib/screens/birthday/birthdays.screen.dart
+++ lib/screens/birthday/birthdays.screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:geburtstags_app/models/birthday.dart';
 import 'package:geburtstags_app/repositories/birthday.repo.dart';
-import 'package:geburtstags_app/screens/birthday/detail/birthday_detail.screen.dart';
 import 'package:geburtstags_app/screens/birthday/widgets/birthday_form.dart';
+import 'package:grouped_list/grouped_list.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
+
+import 'detail/birthday_detail.screen.dart';
 
 class BirthdaysScreen extends StatelessWidget {
   const BirthdaysScreen({Key? key}) : super(key: key);
@@ -11,55 +14,104 @@ class BirthdaysScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final birthdays = context.watch<BirthdayRepo>().birthdays;
+    List<Birthday> birthdays2 = List.from(birthdays);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Geburtstage"),
       ),
-      body: ListView.builder(
-        itemCount: birthdays.length,
-        itemBuilder: (context, index) {
-          final birthday = birthdays[index];
-          return Dismissible(
-            key: UniqueKey(),
-            direction: DismissDirection.endToStart,
-            background: Container(
-              color: Colors.red,
-              child: const Padding(
-                padding: EdgeInsets.only(right: 10.0),
-                child: Align(
-                  alignment: Alignment.centerRight,
-                  child: Icon(
-                    Icons.delete,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-            onDismissed: (direction) {
-              context.read<BirthdayRepo>().delete(birthday);
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text("${birthday.name} gelöscht.")),
-              );
-            },
-            child: ListTile(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => BirthdayDetailScreen(
-                      birthday: birthday,
+      body: birthdays.isEmpty
+          ? const Center(
+              child: Text("Es stehen keine Geburtstage an"),
+            )
+          : Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: GroupedListView<Birthday, int>(
+                elements: birthdays2,
+                groupBy: (element) => element.date.month,
+                groupSeparatorBuilder: (int groupByValue) {
+                  switch (groupByValue) {
+                    case 1:
+                      return const Text("Januar",
+                          style: TextStyle(fontSize: 20));
+                    case 2:
+                      return const Text("Februar",
+                          style: TextStyle(fontSize: 20));
+                    case 3:
+                      return const Text("März", style: TextStyle(fontSize: 20));
+                    case 4:
+                      return const Text("April",
+                          style: TextStyle(fontSize: 20));
+                    case 5:
+                      return const Text("Mai", style: TextStyle(fontSize: 20));
+                    case 6:
+                      return const Text("Juni", style: TextStyle(fontSize: 20));
+                    case 7:
+                      return const Text("Juli", style: TextStyle(fontSize: 20));
+                    case 8:
+                      return const Text("August",
+                          style: TextStyle(fontSize: 20));
+                    case 9:
+                      return const Text("September",
+                          style: TextStyle(fontSize: 20));
+                    case 10:
+                      return const Text("Oktober",
+                          style: TextStyle(fontSize: 20));
+                    case 11:
+                      return const Text("November",
+                          style: TextStyle(fontSize: 20));
+                    case 12:
+                      return const Text("Dezember",
+                          style: TextStyle(fontSize: 20));
+                    default:
+                      return const Text("Was geht ab",
+                          style: TextStyle(fontSize: 20));
+                  }
+                },
+                itemBuilder: (context, Birthday element) {
+                  final birthday = element;
+                  return Dismissible(
+                    key: UniqueKey(),
+                    direction: DismissDirection.endToStart,
+                    background: Container(
+                      color: Colors.red,
+                      child: const Padding(
+                        padding: EdgeInsets.only(right: 10.0),
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: Icon(
+                            Icons.delete,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                );
-              },
-              title: Text(birthday.name),
-              trailing: Text(
-                DateFormat('dd.MM.yyyy').format(birthday.date),
+                    onDismissed: (direction) {
+                      context.read<BirthdayRepo>().delete(birthday);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("${birthday.name} gelöscht.")),
+                      );
+                    },
+                    child: ListTile(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => BirthdayDetailScreen(
+                              birthday: birthday,
+                            ),
+                          ),
+                        );
+                      },
+                      title: Text(birthday.name),
+                      trailing: Text(
+                        DateFormat('dd.MM.yyyy').format(birthday.date),
+                      ),
+                    ),
+                  );
+                },
               ),
             ),
-          );
-        },
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context).push(
--- lib/screens/dashboard/dashboard.screen.dart
+++ lib/screens/dashboard/dashboard.screen.dart
@@ -51,10 +51,14 @@ class DashboardScreen extends StatelessWidget {
                               child: ListTile(
                                 onTap: () => Navigator.push(
                                   context,
-                                  MaterialPageRoute(builder: (context) => BirthdayDetailScreen(birthday: birthday)),
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          BirthdayDetailScreen(
+                                              birthday: birthday)),
                                 ),
                                 leading: CircleAvatar(
-                                  child: Image.asset("assets/images/default.png"),
+                                  child:
+                                      Image.asset("assets/images/default.png"),
                                   radius: 25,
                                   backgroundColor: Colors.white,
                                 ),
@@ -63,19 +67,24 @@ class DashboardScreen extends StatelessWidget {
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
                                     const SizedBox(height: 5),
-                                    Text("Am ${DateFormat('dd.MM').format(birthday.date)}"),
+                                    Text(
+                                        "Am ${DateFormat('dd.MM').format(birthday.date)}"),
                                     const SizedBox(height: 5),
                                     const Text(
                                       "Heute Geburtstag",
-                                      style: TextStyle(fontStyle: FontStyle.italic, color: Colors.red),
+                                      style: TextStyle(
+                                          fontStyle: FontStyle.italic,
+                                          color: Colors.red),
                                     ),
                                   ],
                                 ),
-                                trailing: Text("$getAge Jahre", style: const TextStyle(fontSize: 18)),
+                                trailing: Text("$getAge Jahre",
+                                    style: const TextStyle(fontSize: 18)),
                               ),
                             ),
                             elevation: 4,
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)),
                           ),
                         );
                       },
@@ -97,7 +106,8 @@ class DashboardScreen extends StatelessWidget {
                       itemBuilder: (context, index) {
                         final dateTimeUtil = DateTimeUtil();
                         final birthday = nextbirthdays[index];
-                        final daysUntilBirthday = dateTimeUtil.remainingDaysUntilBirthday(birthday.date);
+                        final daysUntilBirthday = dateTimeUtil
+                            .remainingDaysUntilBirthday(birthday.date);
                         final getAge = dateTimeUtil.getAge(birthday.date);
 
                         return Padding(
@@ -108,10 +118,14 @@ class DashboardScreen extends StatelessWidget {
                               child: ListTile(
                                 onTap: () => Navigator.push(
                                   context,
-                                  MaterialPageRoute(builder: (context) => BirthdayDetailScreen(birthday: birthday)),
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          BirthdayDetailScreen(
+                                              birthday: birthday)),
                                 ),
                                 leading: CircleAvatar(
-                                  child: Image.asset("assets/images/default.png"),
+                                  child:
+                                      Image.asset("assets/images/default.png"),
                                   radius: 25,
                                   backgroundColor: Colors.white,
                                 ),
@@ -120,19 +134,26 @@ class DashboardScreen extends StatelessWidget {
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
                                     const SizedBox(height: 5),
-                                    Text("Am ${DateFormat('dd.MM').format(birthday.date)}"),
+                                    Text(
+                                        "Am ${DateFormat('dd.MM').format(birthday.date)}"),
                                     const SizedBox(height: 5),
                                     Text(
-                                      daysUntilBirthday == 1 ? "In einem Tag" : "In $daysUntilBirthday Tagen",
-                                      style: TextStyle(fontStyle: FontStyle.italic, color: Colors.green.shade700),
+                                      daysUntilBirthday == 1
+                                          ? "In einem Tag"
+                                          : "In $daysUntilBirthday Tagen",
+                                      style: TextStyle(
+                                          fontStyle: FontStyle.italic,
+                                          color: Colors.green.shade700),
                                     ),
                                   ],
                                 ),
-                                trailing: Text("$getAge Jahre", style: const TextStyle(fontSize: 18)),
+                                trailing: Text("$getAge Jahre",
+                                    style: const TextStyle(fontSize: 18)),
                               ),
                             ),
                             elevation: 4,
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)),
                           ),
                         );
                       },
