641f0a8e30db09563f629d630f26d27f336a8a5d
juliawerner <julia.werner@coodoo.de>
2022-03-25 10:06:49 +0100

    Refactoring

---
+++ lib/ui/screens/base.screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:geburtstags_app/core/viewmodels/base.viewmodel.dart';
+import 'package:geburtstags_app/locator.dart';
+import 'package:provider/provider.dart';
+
+class BaseScreen<T extends BaseViewModel> extends StatefulWidget {
+  final Widget Function(BuildContext context, T model, Widget child) builder;
+  final Function(T) onModelReady;
+
+  BaseScreen(this.builder, this.onModelReady);
+
+  @override
+  _BaseScreenState<T> createState() => _BaseScreenState<T>();
+}
+
+class _BaseScreenState<T extends BaseViewModel> extends State<BaseScreen<T>> {
+  T model = locator<T>();
+
+  @override
+  void initState() {
+    if (widget.onModelReady != null) {
+      widget.onModelReady(model);
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<T>(
+      create: (BuildContext context) => model,
+      child: Consumer<T>(builder: widget.builder),
+    );
+  }
+}
