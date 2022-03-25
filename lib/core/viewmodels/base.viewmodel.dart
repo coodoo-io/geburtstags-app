6be7f9fe452310bb243c160ac28681f013e81dd9
Jan Marsh <Jan.Marsh@coodoo.de>
2022-03-25 09:36:34 +0100

    fix it

--- lib/core/viewmodels/base.viewmodel.dart
+++ lib/core/viewmodels/base.viewmodel.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:geburtstags_app/core/enum/view_state.dart';
 
 class BaseViewModel extends ChangeNotifier {
-  ViewState _state = ViewState.Idle;
+  ViewState _state = ViewState.idle;
 
   ViewState get state => _state;
 
