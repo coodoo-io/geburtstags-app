55c9b989c48efa2cc38bff52fc0a7f979e7c12fb
Marcel Ploch <ploch@MBP-von-Marcel.localdomain>
2022-07-19 13:57:14 +0200

    Fixing api response

--- lib/repositories/birthday.repo.dart
+++ lib/repositories/birthday.repo.dart
@@ -8,10 +8,12 @@ import 'package:http/http.dart' as http;
 
 extension StringCapitalizeExtension on String {
   /// Makes the first letter of a string uppercase.
-  String get toCapitalized => length > 0 ? '${this[0].toUpperCase()}${substring(1).toLowerCase()}' : '';
+  String get toCapitalized =>
+      length > 0 ? '${this[0].toUpperCase()}${substring(1).toLowerCase()}' : '';
 
   /// Makes every word's first letter uppercase.
-  String get capitalizeFirstofEach => split(" ").map((str) => str.toCapitalized).join(" ");
+  String get capitalizeFirstofEach =>
+      split(" ").map((str) => str.toCapitalized).join(" ");
 }
 
 class BirthdayRepo extends ChangeNotifier {
@@ -28,15 +30,18 @@ class BirthdayRepo extends ChangeNotifier {
   final List<Birthday> _birthdays = [];
   List<Birthday> _celebrityBirthdays = [];
 
-  UnmodifiableListView<Birthday> get birthdays => UnmodifiableListView(_birthdays);
-  UnmodifiableListView<Birthday> get celebrityBirthdays => UnmodifiableListView(_celebrityBirthdays);
+  UnmodifiableListView<Birthday> get birthdays =>
+      UnmodifiableListView(_birthdays);
+  UnmodifiableListView<Birthday> get celebrityBirthdays =>
+      UnmodifiableListView(_celebrityBirthdays);
 
   List<Birthday> getNextFiveBirthdays() {
     final dateTimeUtil = DateTimeUtil();
     List<Birthday> nextFiveBirthdays = List.from(_birthdays);
 
-    nextFiveBirthdays.sort((a, b) =>
-        dateTimeUtil.remainingDaysUntilBirthday(a.date).compareTo(dateTimeUtil.remainingDaysUntilBirthday(b.date)));
+    nextFiveBirthdays.sort((a, b) => dateTimeUtil
+        .remainingDaysUntilBirthday(a.date)
+        .compareTo(dateTimeUtil.remainingDaysUntilBirthday(b.date)));
 
     if (nextFiveBirthdays.length > 5) {
       return nextFiveBirthdays.sublist(0, 5);
@@ -48,7 +53,8 @@ class BirthdayRepo extends ChangeNotifier {
     List<Birthday> list = [];
 
     for (var i = 0; i < _birthdays.length; i++) {
-      if (_birthdays[i].date.day == DateTime.now().day && _birthdays[i].date.month == DateTime.now().month) {
+      if (_birthdays[i].date.day == DateTime.now().day &&
+          _birthdays[i].date.month == DateTime.now().month) {
         list.add(_birthdays[i]);
       }
     }
@@ -86,30 +92,35 @@ class BirthdayRepo extends ChangeNotifier {
       final List decodedList = jsonDecode(response.body);
       for (var element in decodedList) {
         Birthday birthday;
+
         // Ignore if birthday is null
-        if (element["birthdy"] == null) {
+
+        if (element["birthday"] == null) {
         }
         // If only the birth year is given
-        else if (element["birthdy"].length == 4) {
+        else if (element["birthday"].length == 4) {
           birthday = Birthday(
             name: element["name"].toString().capitalizeFirstofEach,
-            date: DateTime(int.parse(element["birthdy"])),
+            date: DateTime(int.parse(element["birthday"])),
           );
           celebrityList.add(birthday);
         } else {
           birthday = Birthday(
             name: element["name"].toString().capitalizeFirstofEach,
-            date: DateTime.parse(element["birthdy"]),
+            date: DateTime.parse(element["birthday"]),
           );
+
           celebrityList.add(birthday);
         }
       }
 
-      celebrityList.sort((a, b) =>
-          dateTimeUtil.remainingDaysUntilBirthday(a.date).compareTo(dateTimeUtil.remainingDaysUntilBirthday(b.date)));
+      celebrityList.sort((a, b) => dateTimeUtil
+          .remainingDaysUntilBirthday(a.date)
+          .compareTo(dateTimeUtil.remainingDaysUntilBirthday(b.date)));
     }
 
-    _celebrityBirthdays = celebrityList;
+    _celebrityBirthdays = celebrityList.sublist(0, 5);
+
     notifyListeners();
   }
 }
